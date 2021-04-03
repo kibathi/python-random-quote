a2b0b5f469ae08753a3147e2cd82d07720b1b99b
kibathi <bernardkibathi91@gmail.com>
2021-04-03 11:29:35 +0300

    Random quote bot funtional

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,17 @@
+import random
 def pri():
-  print("Keep it logically awesome.")
+  # print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  getlast = len(quotes)
+  last = len(quotes)-1
+  rnd = random.randint(0,last)
 
-  #print(quotes)
+  # print(getlast)
+  print(quotes[rnd])
+
 
 if __name__== "__main__":
   pri()
